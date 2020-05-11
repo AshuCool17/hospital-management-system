48a388a60220c9ac634d229ca63c8702ac033c02
Ashutosh <ashutosh.mahato@gmail.com>
2020-05-11 22:31:49 +0530

    Added comment in all the tests in PatientControllerTest

--- hospital-management-service/src/test/java/com/services/controller/PatientControllerTest.java
+++ hospital-management-service/src/test/java/com/services/controller/PatientControllerTest.java
@@ -5,10 +5,9 @@ package com.services.controller;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +18,6 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -55,61 +53,60 @@ public class PatientControllerTest extends AbstractControllerTest{
 	@Test
 	public void testAddPatient() throws Exception {
 		
-		Patient patient = preparePatientData();
+		Patient patient = preparePatientData(); // creating a mock object
 		String patientJson = this.mapToJson(patient);
 		
-		Mockito.when(patientService.addPatient(any())).thenReturn(patient);
+		//Mockito.when(patientService.addPatient(any())).thenReturn(patient); //whenever the addPatient API is called, return the mocked patient object 
 		
 		// Make request and verify it was successful
 		mvc.perform(MockMvcRequestBuilders.post("/addPatient")
 									.accept(MediaType.APPLICATION_JSON)
 									.contentType(MediaType.APPLICATION_JSON).content(patientJson))
-									.andExpect(status().isCreated()).andReturn();
+									.andExpect(status().isCreated()).andReturn(); //verifying 201 status as response
 		
-		Mockito.verify(patientService, Mockito.times(1)).addPatient(any());
+		Mockito.verify(patientService, Mockito.times(1)).addPatient(any()); //verifying the service method executed once and retrieved back the result
 	}
 	
 	@Test
 	public void testDeletePatient() throws Exception {
 		
-		Patient patient = preparePatientData();
+		Patient patient = preparePatientData();  // creating a mock object
 		String patientJson = this.mapToJson(patient);
 		long patientId = 1;
 		
-		Mockito.when(patientService.findPatientById(anyLong())).thenReturn(patient);
+		Mockito.when(patientService.findPatientById(anyLong())).thenReturn(patient); //surpasses the findPatientId and feeds in the mocked patient object
 		
 		// Make request and verify it was successful
 		mvc.perform(MockMvcRequestBuilders.delete("/deletePatientById/{patientId}", patientId)
 										.accept(MediaType.APPLICATION_JSON)
 										.contentType(MediaType.APPLICATION_JSON).content(patientJson))
-										.andExpect(status().isNoContent()).andReturn();
+										.andExpect(status().isNoContent()).andReturn(); //verifying 204 status as response
 		
-		Mockito.verify(patientService, Mockito.times(1)).deletePatientById(anyLong());
+		Mockito.verify(patientService, Mockito.times(1)).deletePatientById(anyLong()); //verifying the service method executed once and retrieved back the result
 	}
 	
 	@Test
 	public void testFindAllPatients() throws Exception {
 		List<Patient> patientList = new ArrayList<>();
-		Patient patient = preparePatientData();
-		patientList.add(patient);
+		Patient patient = preparePatientData();  // creating a mock object
+		patientList.add(patient); // add to the patientList
 		
-		patient = preparePatientData();
-		patientList.add(patient);
+		patient = preparePatientData();  // creating a mock object
+		patientList.add(patient); // add to the patientList
 		
 		Mockito.when(patientService.findAllPatients()).thenReturn(patientList);
 		
 		// Make request and verify it was successful
 		MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/findAllPatients")
 									.accept(MediaType.APPLICATION_JSON))
-									.andExpect(status().isOk()).andReturn();
+									.andExpect(status().isOk()).andReturn();  //verifying 200 status as response
 				
-		Patient[] responsePatients = mapFromJson(result.getResponse().getContentAsString(), Patient[].class);
+		Patient[] responsePatients = mapFromJson(result.getResponse().getContentAsString(), Patient[].class); //convert the response into a patient array for size comparison
 		List<Patient> responsePatientList = new ArrayList<>();
 		for (Patient responsePatient : responsePatients) {
 			responsePatientList.add(responsePatient);
 		}
-		assertThat(responsePatients.length, is(2));
-		assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
+		assertThat(responsePatientList.size(), is(2)); //check if the response has 2 patient records or not
 		
 	}
 	
@@ -124,14 +121,13 @@ public class PatientControllerTest extends AbstractControllerTest{
 		// Make request and verify it was successful
 		MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/findPatientById/{patientId}", patientId)
 									.accept(MediaType.APPLICATION_JSON))
-									.andExpect(status().isOk()).andReturn();
+									.andExpect(status().isOk()).andReturn();  //verifying 200 status as response
 				
-		Patient responsePatient = mapFromJson(result.getResponse().getContentAsString(), Patient.class);
+		Patient responsePatient = mapFromJson(result.getResponse().getContentAsString(), Patient.class); //convert the response into a patient object to be used for verification if the values were same as the mocked object
 		
-		assertThat(responsePatient.getFirstName(), is("Ashutosh"));
-		assertThat(responsePatient.getLastName(), is("Mahato"));
-		assertThat(responsePatient.getAge(), is(30));
-		assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
+		assertThat(responsePatient.getFirstName(), is("Ashutosh")); //check if the response has the patient first name as "Ashutosh"
+		assertThat(responsePatient.getLastName(), is("Mahato")); //check if the response has the patient last name as "Mahato"
+		assertThat(responsePatient.getAge(), is(30)); //check if the response has the patient age as 30
 		
 	}
 
