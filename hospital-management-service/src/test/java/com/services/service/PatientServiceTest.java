00208ebb5fd0fb9a14593662af6a7595d97f2ab7
Ashutosh <ashutosh.mahato@gmail.com>
2020-05-19 23:47:02 +0530

    Modified the name of test case

--- hospital-management-service/src/test/java/com/services/service/PatientServiceTest.java
+++ hospital-management-service/src/test/java/com/services/service/PatientServiceTest.java
@@ -28,11 +28,7 @@ public class PatientServiceTest {
 	}
 	
 	@Test
-	public void testVerifyRoleForUnauthenticatedRoleShouldReturnTrue() throws GlobalException {
-		String email = "Email@email.com";
-		String password = "P455W#RD";
-		String basicAuth = "Basic " + Base64Utils.encodeToString((email + ":" + password).getBytes());
-		String IP = "192.168.1.1";
+	public void testAddPatient() throws GlobalException {
 		List<String> resetRole = new ArrayList<String>();
 		resetRole.add(Constants.UNVERIFIED_ROLE);
 		assertThat(roleService.verifyRoles(basicAuth, IP, resetRole, Permission.PASSWORD_UPDATE), is(true));
