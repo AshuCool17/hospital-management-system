9cb95659c3b23647975325c1d31cbd822d7e25b5
Ashutosh <ashutosh.mahato@gmail.com>
2020-05-26 21:30:01 +0530

    Working upon test case for delete operation in service layer

--- hospital-management-service/src/test/java/com/services/service/PatientServiceTest.java
+++ hospital-management-service/src/test/java/com/services/service/PatientServiceTest.java
@@ -92,6 +92,18 @@ public class PatientServiceTest {
 		
 	}
 	
+	@Test
+	public void testDeletePatientById() {
+		long patientId = 1L;
+		Patient patient = preparePatientData();
+		patient.setId(patientId);
+		//when(patientRepository.findById(patientId).isPresent()).thenReturn(true);
+		//doReturn(true).when(patientService).deletePatientById(patientId);
+		patientService.deletePatientById(patientId);
+		
+		verify(patientRepository, times(1)).deleteById(patientId);
+	}
+	
 	private Patient preparePatientData() {
 		Patient patient = new Patient();
 		patient.setFirstName("Ashutosh");
