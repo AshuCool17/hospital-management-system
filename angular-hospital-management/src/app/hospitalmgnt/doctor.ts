efba7907d6ad717e9b37d8feff4cf52f3d4678da
Ashutosh <ashutosh.mahato@gmail.com>
2020-07-09 18:32:23 +0530

    Added doctor model class

---
+++ angular-hospital-management/src/app/hospitalmgnt/doctor.ts
@@ -0,0 +1,11 @@
+export class Doctor{
+    id : number;
+    firstName : string;
+    lastName : string;
+    age : number;
+    gender : string;
+    mobileNumber : number;
+    emailId : string;
+    specialization : string;
+    imageUrl : string;
+}
