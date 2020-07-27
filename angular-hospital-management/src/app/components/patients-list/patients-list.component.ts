c107bcfcf20f758b4948b5fa6649e023a02f5236
Ashutosh <ashutosh.mahato@gmail.com>
2020-07-27 22:53:37 +0530

    Added comment for add Patient API

--- angular-hospital-management/src/app/components/patients-list/patients-list.component.ts
+++ angular-hospital-management/src/app/components/patients-list/patients-list.component.ts
@@ -25,6 +25,7 @@ export class PatientsListComponent implements OnInit {
     )
   }
 
+  /* add patient */
   addPatients(){
     this.patientService.addorUpdatePatient(
       data => this.patients = data
