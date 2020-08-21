5eb193a95b8a1e6b8347a31cb3e6033f1a38d6f6
Ashutosh <ashutosh.mahato@gmail.com>
2020-08-21 18:29:36 +0530

    Added comment for home page navigation

--- angular-hospital-management/src/app/login/login.component.ts
+++ angular-hospital-management/src/app/login/login.component.ts
@@ -24,7 +24,7 @@ export class LoginComponent implements OnInit{
         if (this.loginservice.authenticate(this.username, this.password)) {
             
             console.log("Authentic user");
-            this.router.navigate([''])
+            this.router.navigate(['']) //navigate to the home page if the credentials are authentic
             this.invalidLogin = false //valid user
         } 
         else{
