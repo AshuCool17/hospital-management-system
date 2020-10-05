07d352f085b813f93f07d8b769b971cd993e9e73
Ashutosh <ashutosh.mahato@gmail.com>
2020-10-05 22:21:29 +0530

    Added spaces

--- angular-hospital-management/src/app/components/patients-list/patients-list.component.ts
+++ angular-hospital-management/src/app/components/patients-list/patients-list.component.ts
@@ -44,6 +44,7 @@ export class PatientsListComponent implements OnInit {
 
   /* delete patient by id */
   deletePatientById(id) {
+    
     this.patientService.deletePatientById(id).subscribe(      
     )
   }
