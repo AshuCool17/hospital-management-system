9fd0ccac781bc52f47ad8a1d42d0fe3ba01c0b7e
Ashutosh <ashutosh.mahato@gmail.com>
2020-12-07 09:49:46 +0530

    Removed space

--- angular-hospital-management/src/app/service/auth-guard.service.ts
+++ angular-hospital-management/src/app/service/auth-guard.service.ts
@@ -12,8 +12,7 @@ export class AuthGuardService implements CanActivate{
     }
     /* user authentication */
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {    
-    console.log('Entered canActivate method');    
-    
+    console.log('Entered canActivate method');
     if (this.authService.isUserLoggedIn()){ //if logged-in user
       console.log('Authenticated user');
       this.router.navigate(['login']); //navigating to login page
