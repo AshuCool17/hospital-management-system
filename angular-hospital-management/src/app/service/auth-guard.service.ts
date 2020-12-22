9b8f759da0b923cd0e77240026523d4e969194bb
Ashutosh <ashutosh.mahato@gmail.com>
2020-12-22 22:24:02 +0530

    Added space in between

--- angular-hospital-management/src/app/service/auth-guard.service.ts
+++ angular-hospital-management/src/app/service/auth-guard.service.ts
@@ -8,20 +8,25 @@ import { AuthenticationService } from './authentication.service';
 export class AuthGuardService implements CanActivate{
 
   constructor(private router: Router,
-    private authService: AuthenticationService) {                  
+    private authService: AuthenticationService) {  
+
     }
     /* user authentication */
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {  
     
     console.log('Entered canActivate method'); 
     if (this.authService.isUserLoggedIn()){ //if logged-in user
+
       console.log('Authenticated user');
       this.router.navigate(['login']); //navigating to login page
       console.log('Navigate to login page');     
       return true; //if authenticated user, return true 
+    
     } else {      
+    
       console.log('Unathenticated user');
       return false; //if unauthenticated user, return false      
+    
     }
     
   }
