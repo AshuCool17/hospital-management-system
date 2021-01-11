b73b142fbd9931d246f9c9d47d7624cb991ddac7
Ashutosh <ashutosh.mahato@gmail.com>
2021-01-11 20:48:30 +0530

    Removed space

--- angular-hospital-management/src/app/service/auth-guard.service.ts
+++ angular-hospital-management/src/app/service/auth-guard.service.ts
@@ -23,10 +23,8 @@ export class AuthGuardService implements CanActivate{
       return true; //if authenticated user, return true 
     
     } else {      
-    
       console.log('Unathenticated user');
       return false; //if unauthenticated user, return false      
-    
     }
   }
 }
