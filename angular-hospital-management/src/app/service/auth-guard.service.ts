82303928997fe510572f21ffcf0b1b0e3edc211b
Ashutosh <ashutosh.mahato@gmail.com>
2021-03-03 22:55:24 +0530

    Added space

--- angular-hospital-management/src/app/service/auth-guard.service.ts
+++ angular-hospital-management/src/app/service/auth-guard.service.ts
@@ -9,6 +9,8 @@ export class AuthGuardService implements CanActivate{
 
   constructor(private router: Router,
     private authService: AuthenticationService) {
+      
+
     }
     /* user authentication */
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {    
